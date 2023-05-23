1fa6753ed03098ec99c4fb24201fc3d8f0f0ed66
ilyadmtr <ilyadmytrenko@gmail.com>
2023-05-23 23:01:11 +0200

    redirect to login

--- src/lib/routes.js
+++ src/lib/routes.js
@@ -1,4 +1,4 @@
-import {createBrowserRouter} from "react-router-dom";
+import {createBrowserRouter, Navigate, redirect} from "react-router-dom";
 import Login from "../components/auth/Login";
 import Register from "../components/auth/Register";
 import Layout from "../components/layout";
@@ -12,22 +12,37 @@ import Users from "../components/users";
 export const ROOT = "/";
 export const LOGIN = "/login";
 export const REGISTER = "/register";
+
+export const PROTECTED = "/protected";
 export const DASHBOARD = "/protected/dashboard";
 export const USERS = "/protected/users";
 export const PROFILE = "/protected/profile/:id";
 export const COMMENTS = "/protected/comments/:id";
-export const PROTECTED = "/protected";
 
 export const router = createBrowserRouter([
-    {path: ROOT, element: "Public root"},
-    {path: LOGIN, element: <Login/>},
-    {path: REGISTER, element: <Register/>},
-    {path: PROTECTED, element: <Layout/>,
-        children:
-            [
-                {path: DASHBOARD, element: <Dashboard/>},
-                {path: USERS, element: <Users/>},
-                {path: PROFILE, element: <Profile/>},
-                {path: COMMENTS, element: <Comments/>},
-            ]},
-])
+    { path: ROOT, element: <Navigate to={'/login'} replace/> },
+    { path: LOGIN, element: <Login /> },
+    { path: REGISTER, element: <Register /> },
+    {
+        path: PROTECTED,
+        element: <Layout />,
+        children: [
+            {
+                path: DASHBOARD,
+                element: <Dashboard />,
+            },
+            {
+                path: USERS,
+                element: <Users />,
+            },
+            {
+                path: PROFILE,
+                element: <Profile />,
+            },
+            {
+                path: COMMENTS,
+                element: <Comments />,
+            },
+        ],
+    },
+]);
